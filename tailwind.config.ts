8be9101af98d8a35701b1c2a897e827fc6c2f12e
Paulius G <paulius@2simple.com>
2024-12-30 13:43:27 +0000

    Update homepage layout and add background image configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
   	extend: {
+		backgroundImage: {
+			'home-img': "url('/images/home-img.jpg')"
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
